832eb4efdd10357d4beed39ef6aaf4d89fd43e50
ag2byte1 <capsand123@outlook.com>
2021-02-24 17:04:19 +0530

    [UPDATE] Added tick to item

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,11 +1,15 @@
 // this file is for one todo, we can loop through in order to show all
 import React from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
+import { TiTick } from "react-icons/ti";
 import "../Components/Todo.css";
 const Todo = () => {
   return (
     <ListGroup className="mt-5 mb-2 Todo">
-      <ListGroupItem>span</ListGroupItem>
+      <ListGroupItem className="lgitem">
+        <span>Todo</span>
+        <TiTick></TiTick>
+      </ListGroupItem>
     </ListGroup>
   );
 };
